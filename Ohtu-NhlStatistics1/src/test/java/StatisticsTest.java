41fc0878e4e8ecc48b4afabf90e4ee30f2cc1824
tiaros <timo.a.jaaskelainen@helsinki.fi>
2015-03-20 16:21:35 +0200

    viikko2

--- Ohtu-NhlStatistics1/src/test/java/StatisticsTest.java
+++ Ohtu-NhlStatistics1/src/test/java/StatisticsTest.java
@@ -1,45 +1,41 @@
 
 import java.util.ArrayList;
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
-import java.util.Scanner;
 import java.util.List;
-import ohtuesimerkki.*;
-import java.util.*;
+import ohtuesimerkki.Player;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import ohtuesimerkki.Reader;
+import ohtuesimerkki.Statistics;
 
 public class StatisticsTest{
 
-  Reader readerStub = new Reader() {
-         public List<Player> getPlayers() {
+   Statistics stats;
+    Reader readerStub = new Reader() {
+
+        public List<Player> getPlayers() {
             ArrayList<Player> players = new ArrayList<Player>();
- 
+
             players.add(new Player("Semenko", "EDM", 4, 12));
             players.add(new Player("Lemieux", "PIT", 45, 54));
             players.add(new Player("Kurri",   "EDM", 37, 53));
             players.add(new Player("Yzerman", "DET", 42, 56));
             players.add(new Player("Gretzky", "EDM", 35, 89));
- 
+
             return players;
         }
+
+
     };
-
-class Readerstub implements Reader {
-
-  ArrayList<Player> players;
-
-  public Readerstub(ArrayList<Player> players) {
-    this.players = players;
-
-}
-  public List<Player> getPlayers() {
-
-  return players;
-
-}
-
-}
-  Statistics stats = new Statistics(readerStub);
-
+    
+   @Before
+    public void setUp() {
+        stats = new Statistics(readerStub);
+    }
+    
   @Test
   public void loytyvaHaku() {
     Player player = stats.search("Semenko");
@@ -53,7 +49,7 @@ class Readerstub implements Reader {
     assertEquals(null, stats.search("trolli"));
 }
   @Test
-  public void tiiminListaaminen() {
+  public void joukkoeenListaaminen() {
     assertEquals("Lemieux", stats.team("PIT").get(0).getName());
 
 }
