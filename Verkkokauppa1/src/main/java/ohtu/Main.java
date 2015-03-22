bc7d4dd7188ab37b4b5b76fb4a7fb9da204fa814
tiaros <timo.a.jaaskelainen@helsinki.fi>
2015-03-22 14:42:34 +0200

    teht 8

--- Verkkokauppa1/src/main/java/ohtu/Main.java
+++ Verkkokauppa1/src/main/java/ohtu/Main.java
@@ -6,7 +6,7 @@ import ohtu.verkkokauppa.Kirjanpito;
 public class Main {
 
     public static void main(String[] args) {
-        Kauppa kauppa = new Kauppa();
+        Kauppa kauppa = new Kauppa(Varasto.getInstance(), Pankki.getInstance(), Viitegeneraattori.getInstance() );
 
         // kauppa hoitaa yhden asiakkaan kerrallaan seuraavaan tapaan:
         kauppa.aloitaAsiointi();
--- Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
+++ Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
@@ -8,22 +8,25 @@ public class Kauppa {
     private Viitegeneraattori viitegeneraattori;
     private String kaupanTili;
 
-    public Kauppa() {
-        varasto = Varasto.getInstance();
-        pankki = Pankki.getInstance();
-        viitegeneraattori = Viitegeneraattori.getInstance();
+    public Kauppa(Varasto varasto, Pankki pankki, Viitegeneraattori viitegeneraattori) {
+        this.varasto = varasto;
+        this.pankki = pankki;
+        this.viitegeneraattori = viitegeneraattori;
         kaupanTili = "33333-44455";
     }
 
+    
     public void aloitaAsiointi() {
         ostoskori = new Ostoskori();
     }
 
+  
     public void poistaKorista(int id) {
         Tuote t = varasto.haeTuote(id); 
         varasto.palautaVarastoon(t);
     }
 
+   
     public void lisaaKoriin(int id) {
         if (varasto.saldo(id)>0) {
             Tuote t = varasto.haeTuote(id);             
@@ -31,6 +34,7 @@ public class Kauppa {
             varasto.otaVarastosta(t);
         }
     }
+
 
     public boolean tilimaksu(String nimi, String tiliNumero) {
         int viite = viitegeneraattori.uusi();
